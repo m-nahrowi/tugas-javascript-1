5cf0fc21de40446173c914060f18e2f9a5d12172
nahrowi.one <86625224+m-nahrowi@users.noreply.github.com>
2022-05-30 16:35:23 +0700

    Update tugas_2.js

--- tugas_2.js
+++ tugas_2.js
@@ -5,23 +5,27 @@ const bahasaIndonesia = 70;
 const bahasaInggris = 89;
 const ipa = 69;
 
+let grade = "";
+
 if (mtk&&bahasaIndonesia&&bahasaInggris&&ipa !== null){
 
     const nilai_rata_rata = (mtk+bahasaIndonesia+bahasaInggris+ipa)/4;
-    console.log("Rata-rata = "+nilai_rata_rata);
+
     if (nilai_rata_rata >= 90 && nilai_rata_rata <=100){
-        console.log("Grade A")
+        grade = "A";
     } else if (nilai_rata_rata>=80 && nilai_rata_rata <=89){
-        console.log("Grade B")
+        grade = "B";
     } else if (nilai_rata_rata>=70 && nilai_rata_rata <= 79){
-        console.log("Grade C")
+        grade = "C";
     } else if (nilai_rata_rata>=60 && nilai_rata_rata <= 69){
-        console.log("Grade D")
+        grade = "D";
     } else if (nilai_rata_rata>=0 && nilai_rata_rata <= 59){
-        console.log("Grade E")
+        grade = "E";
     } else{
         console.log("Ada yang salah dari ujian anda")
+        grade = "undefined"
     }
+    console.log(`Rata-rata = ${nilai_rata_rata} \nGrade = ${grade}`);
  }else {
     console.log("Ada data yang belum diisi")
 }
